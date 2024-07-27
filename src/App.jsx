f5d6d3f020ec54399862c1767a8e97cf565ca7db
AyushIIITU <ayush01122004@gamil.com>
2024-07-27 16:59:16 +0530

    add 3d profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 
 import './App.css'
+import ProfileV1 from './Components/3D-Profile-Card/3D-Profile'
+import Header from './Header'
 
 function App() {
 
   return (
     <>
-     
+     <Header/>
+     <ProfileV1/>
     </>
   )
 }
